d1cc46bc91ba68ee9240f2202db25b70de656841
nitesh kakkar <kakkar.nitesh04@gmail.com>
2024-12-11 20:00:22 +0530

    feat: navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Well Hello</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -18,5 +22,10 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
     fontSize: 24,
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
   },
 });
